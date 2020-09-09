4e1c2f127ec11db26246d55087cfdaac9950f2e1
lele <lele748916898@outlook.com>
2020-09-09 09:13:59 +0800

    Update AudioStack

--- Assets/Scripts/Audio track/AudioSourceManager.cs
+++ Assets/Scripts/Audio track/AudioSourceManager.cs
@@ -1,23 +1,66 @@
-﻿using System.Linq;
+﻿using System.Security.Principal;
+using System.Net.Mime;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.UI;
 
 public class AudioSourceManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    [FoldoutGroup("ListFiles")]
+    [InfoBox("文件列表")]
+    [Tooltip("音频储存路径")]
     public string path;
+
+    [FoldoutGroup("ListFiles")]
+    [Tooltip("音频数组")]
     public List<string> audioSources;
+
+    [FoldoutGroup("ListFiles")]
+    [Tooltip("音频数组实例化模板")]
+    public Image examplePrefabImage;
+
+
+    [FoldoutGroup("ListFiles")]
+    [Tooltip("实例化模板父物体位置")]
+    public Transform examplePrefabParent;
+
+
+    [FoldoutGroup("AudioStackUI")]
+    [Tooltip("音轨时间轴间距控制器")]
+    public GridLayoutGroup gridLayoutGroup;
+
+    [FoldoutGroup("AudioStackUI")]
+    [Tooltip("音轨时间轴间隔数组")]
+    public List<GameObject> audioStackTimes;
+
+    [FoldoutGroup("AudioStackUI")]
+    [Tooltip("音轨时间轴间距UI模板")]
+    public GameObject audioStackUI;
+
+    [FoldoutGroup("AudioStackUI")]
+    [Tooltip("音轨时间轴数量")]
+    public int audioStackCount;
+
+    [FoldoutGroup("AudioStackUI")]
+    [Tooltip("音轨时间轴缩放比例")]
+    public float audioStackSpaceX;
+
     void Start()
     {
         LoadAudioSources(path);
+        AudioStackUiProduction();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AudioSourceStackIntervalChange();
+        print(gridLayoutGroup.spacing.x+"------");
     }
 
     /// <summary>
@@ -36,8 +79,92 @@ public class AudioSourceManager : MonoBehaviour
                 if(fileInfos[i].Name.Split('.').Length<3)
                 {
                     audioSources.Add(fileInfos[i].Name);
+                    var examplesPrefabImages=Instantiate(examplePrefabImage);
+                    examplesPrefabImages.transform.SetParent(examplePrefabParent);
+                    examplesPrefabImages.name=fileInfos[i].Name;
+                    examplesPrefabImages.GetComponentInChildren<Text>().text=fileInfos[i].Name;
+                    //点击事件
+                    examplesPrefabImages.GetComponentInChildren<Button>().onClick.AddListener(delegate{OnClickBtnExamplePrefabImages();});
                 }
             }
         }
     }
+
+    public void  AudioStackUiProduction()
+    {
+        for(int i=0;i<audioStackCount;i++)
+        {
+            //音轨UI间隔
+            var auioStackUIs=Instantiate(audioStackUI);
+            auioStackUIs.GetComponentInChildren<Text>().text=i.ToString();
+            auioStackUIs.name=auioStackUIs.name+"_"+i;
+            auioStackUIs.gameObject.transform.SetParent(gridLayoutGroup.transform);
+            audioStackTimes.Add(auioStackUIs);
+        }
+    } 
+    public void OnClickBtnExamplePrefabImages()
+    {
+        
+    }
+
+    public void AudioSourceStackIntervalChange()
+    {
+        switch  (gridLayoutGroup.spacing.x)
+        {
+            case 45:
+                audioStackSpaceX=2.38f;
+                for(int i=0;i<audioStackTimes.Count;i++)
+                {
+                    if(i%5!=0)
+                    {
+                        audioStackTimes[i].GetComponentInChildren<Text>().enabled=false;
+                        audioStackTimes[i].GetComponentsInChildren<RectTransform>()[2].sizeDelta=new Vector2(1.537476f,12.227f);
+                    }
+                }
+            break;
+
+            case 21.19999f:
+                audioStackSpaceX=1f;
+                for(int i=0;i<audioStackTimes.Count;i++)
+                {
+                    if(i%10!=0)
+                    {
+                        audioStackTimes[i].GetComponentInChildren<Text>().enabled=false;
+                        audioStackTimes[i].GetComponentsInChildren<RectTransform>()[2].sizeDelta=new Vector2(1.537476f,12.227f);
+                    }
+                }
+            break;
+
+            case 4.199989f:
+                 audioStackSpaceX=0;
+                 print(gridLayoutGroup.spacing.x+"------");
+                for(int i=0;i<audioStackTimes.Count;i++)
+                {
+                    if(i%20!=0)
+                    {
+                        audioStackTimes[i].GetComponentInChildren<Text>().enabled=false;
+                        audioStackTimes[i].GetComponentsInChildren<RectTransform>()[2].sizeDelta=new Vector2(1.537476f,6.11f);
+                    }
+                }
+            break;
+        }
+        if(Input.GetAxis("Mouse ScrollWheel")!=0)
+        {
+            if(Input.GetAxis("Mouse ScrollWheel")>0)
+            {
+                if(gridLayoutGroup.spacing.x<1015)
+                {
+                    gridLayoutGroup.spacing=new Vector2(gridLayoutGroup.spacing.x+audioStackSpaceX,0);
+                }
+            }
+            else
+            {
+                if(gridLayoutGroup.spacing.x>5)
+                {
+                    gridLayoutGroup.spacing=new Vector2(gridLayoutGroup.spacing.x-audioStackSpaceX,0);
+                }
+            }
+        }
+    }
+
 }
---
+++ Assets/Scripts/Audio track/AudioSourceTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSourceTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log(other.GetComponentsInParent<Image>()[1].name);
+    }
+}
